1774a46bd8a0c7fcdc0638d1f9f743c9c3e6e944
naveenb13 <naveenb13598@gmail.com>
2022-10-05 20:26:43 +0530

    final commit

--- server.js
+++ server.js
@@ -19,91 +19,6 @@ app.use(cors({
 }))
 
 
-
-
-// let users = [
-//     {
-//         title: "Apple iPhone 13 Mini (128GB) - (Product) RED",
-//         image: "https://m.media-amazon.com/images/I/61F7Xcyux0L._AC_UY218_.jpg",
-//         discount: 64900,
-//         price: 69900,
-//         rating: 4.6,
-//         id: "1"
-//     },
-//     {
-//         title: "Apple iPhone 12 (128GB) - Blue",
-//         image: "https://m.media-amazon.com/images/I/71ZOtNdaZCL._AC_UY218_.jpg",
-//         discount: 54490,
-//         price: 70900,
-//         rating: 4.6,
-//         id: "2"
-//     },
-//     {
-//         title: "Apple iPhone 12 (64GB) - Black",
-//         image: "https://m.media-amazon.com/images/I/71fVoqRC0wL._AC_UY218_.jpg",
-//         discount: 47999,
-//         price: 65900,
-//         rating: 4.6,
-//         id: "3"
-//     },
-//     {
-//         title: "Apple iPhone 12 (128GB) - White",
-//         image: "https://m.media-amazon.com/images/I/711wsjBtWeL._AC_UY218_.jpg",
-//         discount: 54490,
-//         price: 70900,
-//         rating: 4.6,
-//         id: "4"
-//     },
-//     {
-//         title: "Apple iPhone 12 (128GB) - Purple",
-//         image: "https://m.media-amazon.com/images/I/71hIfcIPyxS._AC_UY218_.jpg",
-//         discount: 54490,
-//         price: 70900,
-//         rating: 4.6,
-//         id: "5"
-//     },
-//     {
-//         title: "Apple iPhone 13 Mini (128GB) - (Product) RED",
-//         image: "https://m.media-amazon.com/images/I/61F7Xcyux0L._AC_UY218_.jpg",
-//         discount: 64900,
-//         price: 69900,
-//         rating: 4.6,
-//         id: "6"
-//     },
-//     {
-//         title: "Apple iPhone 12 (128GB) - Blue",
-//         image: "https://m.media-amazon.com/images/I/71ZOtNdaZCL._AC_UY218_.jpg",
-//         discount: 54490,
-//         price: 70900,
-//         rating: 4.6,
-//         id: "7"
-//     },
-//     {
-//         title: "Apple iPhone 12 (64GB) - Black",
-//         image: "https://m.media-amazon.com/images/I/71fVoqRC0wL._AC_UY218_.jpg",
-//         discount: 47999,
-//         price: 65900,
-//         rating: 4.6,
-//         id: "8"
-//     },
-//     {
-//         title: "Apple iPhone 12 (128GB) - White",
-//         image: "https://m.media-amazon.com/images/I/711wsjBtWeL._AC_UY218_.jpg",
-//         discount: 54490,
-//         price: 70900,
-//         rating: 4.6,
-//         id: "9"
-//     },
-//     {
-//         title: "Apple iPhone 12 (128GB) - Purple",
-//         image: "https://m.media-amazon.com/images/I/71hIfcIPyxS._AC_UY218_.jpg",
-//         discount: 54490,
-//         price: 70900,
-//         rating: 4.6,
-//         id: "10"
-//     }
-// ]
-
 let authenticate = (req, res, next) => {
     if (req.headers.authorization) {
         try {
@@ -138,8 +53,6 @@ app.get("/iphone",authenticate, async function (req, res) {
         res.status(500).json({ message: "Something went wrong" })
     }
 
-
-    // res.json(users);
 });
 
 app.post("/iphone",authenticate, async function (req, res) {
@@ -158,12 +71,6 @@ app.post("/iphone",authenticate, async function (req, res) {
     } catch (error) {
         res.status(500).json({ message: "Something went wrong" })
     }
-
-
-    // req.body.id = users.length + 1
-    // users.push(req.body)
-    // console.log(req.body)
-    // res.json({message : "Product created Successfully"})
 });
 
 
